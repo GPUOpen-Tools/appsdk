55a6940ebc963daec69152314a1bb94943287d4c
Amit Prakash <amit_prakash07@hotmail.com>
2019-12-11 15:21:24 -0500

    Remove APP SDK libs

--- Global-OpenCL.cmake
+++
@@ -1,9 +0,0 @@
-## Copyright (c) 2018 Advanced Micro Devices, Inc. All rights reserved.
-cmake_minimum_required(VERSION 3.5.1)
-
-include("Global-OpenCL-NoLib.cmake")
-
-set(OPENCL_LIB "OpenCL.lib")
-
-set(ADDITIONAL_LIBS ${ADDITIONAL_LIBS}
-                    ${OPENCL_LIB})
